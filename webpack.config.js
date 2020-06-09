4cde2ac89cee9557006fa4b3ff295ebc7fca6fc6
107062303 <25349023.qq@gmail.com>
2020-06-09 23:24:22 +0800

    [MISC] webpack settings and packing js

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const srcPath = path.resolve(__dirname, "src");
+const distPath = path.resolve(__dirname, "dist");
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./index.html",
+    filename: "./index.html",
+});
+
+module.exports = {
+    context: srcPath,
+    resolve: {
+        alias: {
+            // utilities: path.resolve(srcPath, "utilities"),
+            // components: path.resolve(srcPath, "components"),
+            // api: path.resolve(srcPath, "api"),
+        },
+    },
+    entry: {
+        index: ["./js/index.js"],
+    },
+    output: {
+        path: distPath,
+        filename: "[name].bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: [/node_modules/],
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                [
+                                    "@babel/preset-env",
+                                    {
+                                        modules: false,
+                                    },
+                                ],
+                                "@babel/preset-react",
+                            ],
+                            plugins: [
+                                "@babel/plugin-proposal-class-properties",
+                                "@babel/plugin-proposal-object-rest-spread",
+                            ],
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "all",
+        },
+    },
+    devServer: {
+        contentBase: distPath,
+        compress: true,
+        port: 7070,
+    },
+    devtool: "cheap-source-map",
+    plugins: [htmlPlugin],
+    watchOptions: {
+        ignored: /node_modules/,
+    },
+};
