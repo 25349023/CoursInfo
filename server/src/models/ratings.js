0874d99fd5af0da5124b68b4232d658d13ca090b
107062303 <25349023.qq@gmail.com>
2020-06-29 20:39:28 +0800

    [ADD] ratings API

---
+++ server/src/models/ratings.js
@@ -0,0 +1,51 @@
+const pgp = require("pg-promise")();
+if (!global.db) {
+    db = pgp(process.env.DB_URL);
+}
+
+function create(userId, options) {
+    const sql = `
+        INSERT INTO ratings(course_semester, course_department,
+            course_subnum, user_id, sweet, cool, recommend)
+        VALUES ($<semester>, $<department>, $<subnumber>,
+            $<userId>, $<sweet>, $<cool>, $<recommend>)
+        RETURNING *;
+    `;
+
+    return db.any(sql, { userId, ...options });
+}
+
+function edit(userId, options) {
+    const sql = `
+        UPDATE ratings
+        SET sweet = $<sweet>,
+            cool = $<cool>,
+            recommend = $<recommend>
+        WHERE user_id = $<userId> AND 
+            (course_semester, course_department, course_subnum) = 
+            ($<semester>, $<department>, $<subnumber>)
+        RETURNING *;
+    `;
+
+    return db.any(sql, { userId, ...options });
+}
+
+function createOrEdit(userId, options) {
+    return create(userId, options).catch((err) => {
+        console.log(err);
+        return edit(userId, options);
+    });
+}
+
+function select(userId, course) {
+    const sql = `
+        SELECT * FROM ratings
+        WHERE user_id = $<userId> AND 
+            (course_semester, course_department, course_subnum) = 
+            ($<semester>, $<department>, $<subnumber>);
+    `;
+
+    return db.any(sql, { userId, ...course });
+}
+
+module.exports = { createOrEdit, select };
